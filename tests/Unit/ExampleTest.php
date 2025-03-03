bd0b7437c25dc9012628ed3530af7ac9ec401fa2
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-03-03 10:22:53 +0300

    tests/Create the job_postings test file

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,41 @@
 <?php
 
-test('that true is true', function () {
-    expect(true)->toBeTrue();
+use App\Models\JobPosting;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+// test('Job postings can be listed', function () {
+//     //list jobs
+//     $job = JobPosting::factory()->create();
+//     JobPosting::factory()->count(3)->create();
+
+//     $response = $this->actingAs($job)
+//                      ->get('/job_listings');
+
+//     $response->assertOk();
+//     // Assert that we get exactly 3 postings in our JSON response
+//     $response->assertJsonCount(3);
+// });
+
+test('Job posting can be created', function () {
+
+    $data = [
+        'title'       => 'Senior Developer',
+        'description' => 'Join our dynamic team to build the future of tech.',
+        'company'     => 'Tech Corp',
+        'location'    => 'Remote',
+        'salary'      => 1000000,
+        'category'    => 'IT',
+    ];
+
+    $response = $this->actingAs($job)
+                     ->post('/job_listings', $data);
+
+    $response->assertStatus(201);
+    $this->assertDatabaseHas('job_postings', [
+        'title' => 'Senior Developer',
+        'company' => 'Tech Corp'
+    ]);
 });
+
